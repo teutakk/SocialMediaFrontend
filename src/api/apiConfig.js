3a6d7e081e0e6874a74c1fd1c3dc756250b36716
endritbejta <endrit.bejta@hotmail.com>
2023-10-05 14:22:10 +0200

    intial commit

---
+++ src/api/apiConfig.js
@@ -0,0 +1,8 @@
+const BASE_URL = "BASE_URL";
+
+const API_ROUTES = {
+  //  define routes here, so we can use them without making spelling mistakes
+  login: "",
+};
+
+export { BASE_URL, API_ROUTES };
