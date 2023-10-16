5730fff0c30236322c1621fe1900315d1c9cef9f
endritbejta <endrit.bejta@hotmail.com>
2023-10-16 13:41:56 +0200

    some changes to api routes

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,7 +1,7 @@
 const BASE_URL = "http://localhost:3001"; // if we include the "/" we have to take care not to include it in routes
 const API_ROUTES = {
   //  define routes here, so we can use them without making spelling mistakes
-  login: "/login",
+  login: "/auth/login",
   register: "/auth/register",
 };
 
