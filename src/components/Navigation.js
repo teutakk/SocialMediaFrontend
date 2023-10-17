ff3c37b52df5fc49f58509f1cd14ef5b224893c6
endritbejta <endrit.bejta@hotmail.com>
2023-10-17 17:32:10 +0200

    added functionality to X that represents hamburger menu , now we can show navigation and hide it, it slides from the left side to the right

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,43 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import classes from "./Navigation.module.css";
 import UserChip from "./UserChip";
 import logo from "../assets/images/starlabs.png";
-const Navigation = () => (
-  <section className={classes.Navigation}>
-    <div className={classes["navigation-left"]}>
-      <span>X</span>
-      <div className={classes.logo}>
-        <img src={logo} alt="lasjdlkasjd" />
-        <h3>Our Social Media</h3>
-      </div>
-    </div>
-    <div className={classes["navigation-main"]}>
-      <label htmlFor="Search...">
-        <input type="text" placeholder="Search...." id="searcg" name="search" />
-      </label>
-      <nav>
-        <ul>
-          <li>
-            <NavLink to="/posts">Feed</NavLink>
-          </li>
-          <li>
-            <NavLink to="/friends">Friends</NavLink>
-          </li>
-          <li>
-            <NavLink to="/profile">Profile</NavLink>
-          </li>
-        </ul>
-      </nav>
-    </div>
 
-    <div className={classes.user}>
-      <div className={classes["notifications-icon"]}>
-        <div className={classes.notifications}>Notification</div>
+const Navigation = () => {
+  const [showNavigation, setShowNavigation] = useState(false);
+  const handleShowNavigation = () => {
+    setShowNavigation((prev) => !prev);
+  };
+  return (
+    <section className={classes.Navigation}>
+      <div className={classes["navigation-left"]}>
+        <span onClick={handleShowNavigation}>X</span>
+        <div className={classes.logo}>
+          <img src={logo} alt="lasjdlkasjd" />
+          <h3>Our Social Media</h3>
+        </div>
       </div>
-      <UserChip url={logo} />
-    </div>
-  </section>
-);
+      <div className={classes["navigation-main"]}>
+        <label htmlFor="Search...">
+          <input
+            type="text"
+            placeholder="Search...."
+            id="searcg"
+            name="search"
+          />
+        </label>
+        <nav className={showNavigation ? classes.open : ""}>
+          <span onClick={handleShowNavigation}>X</span>
+          <ul>
+            <li>
+              <NavLink to="/posts">Feed</NavLink>
+            </li>
+            <li>
+              <NavLink to="/friends">Friends</NavLink>
+            </li>
+            <li>
+              <NavLink to="/profile">Profile</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      <div className={classes.user}>
+        <div className={classes["notifications-icon"]}>
+          <div className={classes.notifications}>Notification</div>
+        </div>
+        <UserChip url={logo} />
+      </div>
+    </section>
+  );
+};
 
 export default Navigation;
