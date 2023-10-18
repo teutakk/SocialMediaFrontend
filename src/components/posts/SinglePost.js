b19c8c0221cbc7d38e2f40337e966028995737f5
Refije <Refijefejzullahu02@gmail.com>
2023-10-18 12:36:35 +0200

    Added a feature for editing posts

--- src/components/posts/SinglePost.js
+++ src/components/posts/SinglePost.js
@@ -1,4 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  startEdit,
+  finishEdit,
+  savePost,
+  selectEditState,
+} from "../../store/slices/postsSlice";
 import PostActions from "./PostActions";
 import PostHeader from "./PostHeader";
 import WritePostComment from "./WritePostComment";
@@ -7,10 +14,48 @@ import classes from "./SinglePost.module.css";
 import PostComments from "./PostComments";
 
 const SinglePost = ({ post }) => {
+  const dispatch = useDispatch();
+  const currentUser = useSelector((state) => state.user);
+
+  const editState = useSelector(selectEditState);
+  const isEditing = editState === post.id; // Check if the post is currently being edited
+
+  const [editedContent, setEditedContent] = useState(post.content);
+
+  const handleEditClick = () => {
+    dispatch(startEdit(post.id)); // Start editing the post
+  };
+
+  const handleEditSave = () => {
+    dispatch(savePost({ id: post.id, content: editedContent })); // Save the edited post
+    dispatch(finishEdit()); // Finish editing
+  };
+
+  const isOwner = currentUser && currentUser.id === post.owner.id;
+
   return (
     <div className={classes.SinglePost}>
       <PostHeader post={post} />
-      <PostContent post={post} />
+      {isEditing ? (
+        <div>
+          <textarea
+            value={editedContent}
+            onChange={(e) => setEditedContent(e.target.value)}
+          />
+          <button onClick={handleEditSave}>Save</button>
+          <button onClick={() => dispatch(finishEdit())}>Cancel</button>
+        </div>
+      ) : (
+        <PostContent post={post} />
+      )}
+
+      {isOwner && !isEditing && (
+        <div>
+          <hr />
+          <button onClick={handleEditClick}>Edit</button>
+        </div>
+      )}
+
       <hr />
       <PostActions />
       <hr />
