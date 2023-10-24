ae418843f23848d2c0d8053d721126a66b741d75
V-Tahiri <veronatahiri02@gmail.com>
2023-10-24 14:58:11 +0200

    Login Design

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,16 +1,39 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { authenticateUser } from "../store/slices/authSlice";
 import { useDispatch } from "react-redux";
 import { useLocation, useParams } from "react-router";
+import { Link, useNavigate } from "react-router-dom";
+import classes from "./styles/Login.module.css";
+import CustomInput from "../components/CustomInput";
+import { AiOutlineMail } from "react-icons/ai";
+import { MdLockOutline } from "react-icons/md";
+import Footer from "../components/Footer";
+
 
 const Login = () => {
   const location = useLocation();
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [formErrors, setFormErrors] = useState({ error: true });
 
   const [formData, setFormData] = useState({
     email: "",
     password: "",
   });
+
+  useEffect(() => {
+    if (formErrors.error !== undefined && !formErrors.error) {
+      dispatch(authenticateUser(formData)).then(() => {
+        navigate("/login");
+      });
+
+      setFormData({
+        email: "",
+        password: "",
+      });
+      setFormErrors({ error: true });
+    }
+  }, [formErrors, formData, dispatch]);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -25,28 +48,50 @@ const Login = () => {
   };
 
   return (
-    <div>
-      {location.hash === "#auth" && <p>You need to login to visit that page</p>}
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="email">Email</label>
-          <input type="email" name="email" id="email" onChange={handleChange} />
+    <div className={classes.container}>
+      <div className={classes.background}></div>
+      <div className={classes.formData}>
+        <div className={classes.topData}>
+          <h2>Log In</h2>
         </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            name="password"
-            id="password"
+        {location.hash === "#auth" && (
+          <p>You need to login to visit that page</p>
+        )}
+        <form onSubmit={handleSubmit} className={classes.form}>
+          <CustomInput
             onChange={handleChange}
+            icon={<AiOutlineMail className={classes.icon} />}
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Enter Your Email"
+            value={formData.email}
           />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-      <div>
-        <p>Dont have an account ?!</p>
-        <a href="register">Sign Up</a>
+          <CustomInput
+            onChange={handleChange}
+            icon={<MdLockOutline className={classes.icon} />}
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Enter Your Password"
+            value={formData.password}
+            label="Password"
+          >
+            {formErrors.password && (
+              <p className={classes.errorText}>{formErrors.password}</p>
+            )}
+          </CustomInput>
+
+          <button type="submit" className={classes.button}>
+            Log In
+          </button>
+          <div className={classes.bottomData}>
+            <p>Don't have an account?</p>
+            <Link to="/register">Sign Up</Link>
+          </div>
+        </form>
       </div>
+      <Footer />
     </div>
   );
 };
