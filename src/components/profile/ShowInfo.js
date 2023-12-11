c3bb429a1cc81f11ffd4297fb7b3e24ee357dae6
Refije <Refijefejzullahu02@gmail.com>
2023-12-11 11:43:03 +0100

    Changes to the style

--- src/components/profile/ShowInfo.js
+++ src/components/profile/ShowInfo.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import classes from "./ShowInfo.module.css";
-import { AiOutlineMenuUnfold } from "react-icons/ai";
 
 const ShowInfo = ({ title, initialContent, onSave, onEditMode = false }) => {
   const [isEditing, setIsEditing] = useState(onEditMode);
@@ -37,7 +36,67 @@ const ShowInfo = ({ title, initialContent, onSave, onEditMode = false }) => {
         <div>
           <p className={classes.content}>{content}</p>
           <div className={classes.dropdown}>
-            <AiOutlineMenuUnfold onClick={toggleEditing} />
+            <button
+              onClick={toggleEditing}
+              style={{
+                display: "flex",
+                alignItems: "center",
+                cursor: "pointer",
+                background: "none",
+                border: "2px #ccc",
+                borderLeft: "4px black ",
+                borderRight: "4px  ",
+                borderRadius: "32px",
+                padding: "0px",
+              }}
+            >
+              <svg
+                width="24px"
+                height="24px"
+                viewBox="-18 -18 60.00 60.00"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                style={{ marginRight: "8px" }}
+              >
+                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                <g
+                  id="SVGRepo_tracerCarrier"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                ></g>
+                <g id="SVGRepo_iconCarrier">
+                  <g clip-path="url(#clip0_429_11139)">
+                    <path
+                      d="M5 16L4 20L8 19L19.5858 7.41421C20.3668 6.63316 20.3668 5.36683 19.5858 4.58579L19.4142 4.41421C18.6332 3.63316 17.3668 3.63317 16.5858 4.41421L5 16Z"
+                      stroke="#292929"
+                      stroke-width="1.8719999999999999"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    ></path>
+                    <path
+                      d="M15 6L18 9"
+                      stroke="#292929"
+                      stroke-width="1.8719999999999999"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    ></path>
+                    <path
+                      d="M13 20H21"
+                      stroke="#292929"
+                      stroke-width="1.8719999999999999"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    ></path>
+                  </g>
+                  <defs>
+                    <clipPath id="clip0_429_11139">
+                      <rect width="24" height="24" fill="white"></rect>
+                    </clipPath>
+                  </defs>
+                </g>
+              </svg>
+              Edit
+            </button>
           </div>
         </div>
       )}
